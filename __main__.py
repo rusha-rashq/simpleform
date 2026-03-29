28a7e9fdebe12f31cee4c1edf65564f67c52b975
rusha-rashq <rushalidhar.911@gmail.com>
2026-03-28 22:12:55 -0400

    feat(storage): implement dynamic region mapping and data versioning

--- __main__.py
+++ __main__.py
@@ -3,24 +3,58 @@ import pulumi_aws as aws
 import pulumi_gcp as gcp
 import yaml
 
-# 1. Load your 'Form'
+# 1. Load the User's Form
 with open("simpleform.yaml", "r") as f:
     config = yaml.safe_load(f)
 
-target_cloud = config.get("cloud", "aws").lower()
-res_name = config.get("name", "simple-res")
+# 2. Regional Mapping Dictionary
+REGION_MAP = {
+    "us-east": {"aws": "us-east-1", "gcp": "us-east1"},
+    "us-west": {"aws": "us-west-2", "gcp": "us-west1"},
+    "europe": {"aws": "eu-central-1", "gcp": "europe-west3"},
+}
 
-# 2. The Simpleform Translation Engine
-if config.get("resource") == "bucket":
-    if target_cloud == "aws":
-        # Maps generic 'bucket' to AWS S3
-        bucket = aws.s3.Bucket(res_name)
-        pulumi.export("bucket_id", bucket.id)
-        pulumi.export("provider", "Amazon Web Services")
+# Extract settings for readability
+settings = config.get("settings", {})
+target_region = REGION_MAP.get(settings.get("region", "us-east"))
+is_public = settings.get("public_access", False)
+use_versioning = settings.get("versioning", False)
 
-    elif target_cloud == "gcp":
-        # Maps generic 'bucket' to GCP Storage
-        # We'll hardcode 'US' for the MVP today
-        bucket = gcp.storage.Bucket(res_name, location="US")
-        pulumi.export("bucket_id", bucket.name)
-        pulumi.export("provider", "Google Cloud Platform")
+# 3. Logic for AWS
+if config["cloud"] == "aws":
+    # Set the region dynamically
+    aws_provider = aws.Provider("aws-provider", region=target_region["aws"])
+
+    bucket = aws.s3.Bucket(
+        config["name"],
+        acl="public-read" if is_public else "private",
+        versioning=aws.s3.BucketVersioningArgs(
+            enabled=use_versioning,
+        ),
+        opts=pulumi.ResourceOptions(provider=aws_provider),
+    )
+
+    pulumi.export("bucket_url", bucket.website_endpoint)
+    pulumi.export("cloud_provider", "Amazon Web Services")
+
+# 4. Logic for GCP
+elif config["cloud"] == "gcp":
+    bucket = gcp.storage.Bucket(
+        config["name"],
+        location=target_region["gcp"],
+        versioning=gcp.storage.BucketVersioningArgs(
+            enabled=use_versioning,
+        ),
+    )
+
+    # If public, we need to add a special IAM binding
+    if is_public:
+        gcp.storage.BucketIAMMember(
+            f"{config['name']}-public-access",
+            bucket=bucket.name,
+            role="roles/storage.objectViewer",
+            member="allUsers",
+        )
+
+    pulumi.export("bucket_url", bucket.self_link)
+    pulumi.export("cloud_provider", "Google Cloud Platform")
